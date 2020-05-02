a4721936bf0877ee6311156a26ed49547e5ed78d
Venkatramans054 <venkatramans054@gmail.com>
2020-05-02 20:56:45 +0530

    second commit

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -11,13 +11,48 @@ import {scan} from 'rxjs/operators'
 export class ChatComponent implements OnInit {
   messages: Observable<Message[]>
   formValue:string
-
+  num: number;
   constructor( private chat: ChatService) { }
 
   ngOnInit(): void {
     this.messages=this.chat.conversation.asObservable().pipe(
       scan((acc,val)=>acc.concat(val))
     );
+    this.num=0
+  }
+
+  getStyle(){
+    if((this.num++)%2){
+      return {
+        'color': 'red',
+        'background-color': '',
+        'font-size': '20px',
+        'border-radius':'25px',
+        'width':'500px',
+        'text-align':'left',
+        'padding-top':'20px',
+        'padding-left':'30px',
+        'padding-bottom':'20px',
+        'font-weight': 700,
+        'border':'black',
+        'border-style': 'double'
+      }
+    }
+    else{
+      return {
+        'color': 'blue',
+        'background-color': '',
+        'border-radius':'25px',
+        'width':'500px',
+        'text-align':'right',
+        'padding-right':'30px',
+        'padding-top':'20px',
+        'padding-bottom':'20px',
+        'font-weight': 700,
+        'border':'black',
+        'border-style': 'double'
+      }
+    }
   }
 
   sendMessage(){
